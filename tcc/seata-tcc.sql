eede787a22d009886b329ac8a4b491cb3a863c65
lyrric <xx20510@163.com>
2020-09-13 18:10:25 +0800

    增加数据库文件

---
+++ tcc/seata-tcc.sql
@@ -0,0 +1,76 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50731
+ Source Host           : localhost:3306
+ Source Schema         : seata-tcc
+
+ Target Server Type    : MySQL
+ Target Server Version : 50731
+ File Encoding         : 65001
+
+ Date: 13/09/2020 18:10:06
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `balance` int(11) NULL DEFAULT NULL COMMENT '余额',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '账户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of account
+-- ----------------------------
+INSERT INTO `account` VALUES (1, 'zhangshan', 3200);
+
+-- ----------------------------
+-- Table structure for account_pre_pay
+-- ----------------------------
+DROP TABLE IF EXISTS `account_pre_pay`;
+CREATE TABLE `account_pre_pay`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `xid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '事务ID',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `money` int(11) NULL DEFAULT NULL COMMENT '预付款金额',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `xid`(`xid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '用户预付款记录' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for invoke_record
+-- ----------------------------
+DROP TABLE IF EXISTS `invoke_record`;
+CREATE TABLE `invoke_record`  (
+  `id` int(11) NOT NULL,
+  `xid` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '事务ID',
+  `function` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作（格式：PAY_PRE、PAY_COMMIT、PAY_ROLLBACK）',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `xid_function`(`xid`, `function`) USING BTREE COMMENT '联合唯一索引'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `status` int(2) NULL DEFAULT NULL COMMENT '状态0=无效，，1=有效',
+  `money` int(11) NULL DEFAULT NULL COMMENT '订单金额',
+  `xid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '事务ID',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
